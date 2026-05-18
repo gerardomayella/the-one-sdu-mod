ace20015b8c87c2f24dd444e89db106346a0d4f8
gerardomayella <127300451+gerardomayella@users.noreply.github.com>
2026-05-18 12:27:59 +0700

    Add vaccine scenario and epidemic ACK handling
    Add a new epidemic_settings.cfg scenario that enables a vaccine/ACK mechanism (Bluetooth interface, 40 hosts, 5M buffers, message generation, reports, TTL and movement settings) and rename the default scenario to include ACK. Implement vaccine logic in EpidemicRouter.receiveMessage: VACCINE messages delete the referenced message from the router's buffer, and normal messages delivered to their final recipient trigger creation of a small ACK message (Type=VACCINE, IdToDelete) back to the sender to prompt deletion. ActiveRouter changes are primarily formatting, Javadoc and whitespace cleanups, small refactors and minor safety/consistency edits; core routing behavior is preserved.
    
    Signed-off-by: gerardomayella <127300451+gerardomayella@users.noreply.github.com>

--- src/routing/EpidemicRouter.java
+++ src/routing/EpidemicRouter.java
@@ -5,52 +5,123 @@
 package routing;
 
 import core.Settings;
+import core.DTNHost;
+import core.Message;
 
 /**
  * Epidemic message router with drop-oldest buffer and only single transferring
  * connections at a time.
  */
 public class EpidemicRouter extends ActiveRouter {
-	
+
 	/**
 	 * Constructor. Creates a new message router based on the settings in
 	 * the given Settings object.
+	 * 
 	 * @param s The settings object
 	 */
 	public EpidemicRouter(Settings s) {
 		super(s);
-		//TODO: read&use epidemic router specific settings (if any)
+		// TODO: read&use epidemic router specific settings (if any)
 	}
-	
+
 	/**
 	 * Copy constructor.
+	 * 
 	 * @param r The router prototype where setting values are copied from
 	 */
 	protected EpidemicRouter(EpidemicRouter r) {
 		super(r);
-		//TODO: copy epidemic settings here (if any)
+		// TODO: copy epidemic settings here (if any)
 	}
-			
+
 	@Override
 	public void update() {
 		super.update();
 		if (isTransferring() || !canStartTransfer()) {
 			return; // transferring, don't try other connections yet
 		}
-		
+
 		// Try first the messages that can be delivered to final recipient
 		if (exchangeDeliverableMessages() != null) {
 			return; // started a transfer, don't try others (yet)
 		}
-		
+
 		// then try any/all message to any/all connection
 		this.tryAllMessagesToAllConnections();
 	}
-	
-	
+
 	@Override
 	public EpidemicRouter replicate() {
 		return new EpidemicRouter(this);
 	}
 
+	// @Override
+	// public int receiveMessage(Message m, DTNHost from) {
+
+	// // menghapus dari
+	// if (m.getProperty("Type") != null && m.getProperty("Type").equals("VACCINE"))
+	// {
+	// String targetId = (String) m.getProperty("IdToDelete");
+	// if (this.hasMessage(targetId)) {
+	// this.deleteMessage(targetId, false);
+	// }
+	// }
+
+	// int recvCheck = checkReceiving(m);
+
+	// if (m.getTo() == this.getHost()) {
+
+	// if (m.getProperty("Type") == null ||
+	// !m.getProperty("Type").equals("VACCINE")) {
+
+	// Message ackMessage = new Message(this.getHost(), m.getFrom(), "ACK_" +
+	// m.getId(), 10);
+
+	// ackMessage.addProperty("Type", "VACCINE");
+	// ackMessage.addProperty("IdToDelete", m.getId());
+
+	// this.createNewMessage(ackMessage);
+	// }
+	// }
+
+	// if (recvCheck != RCV_OK) {
+	// return recvCheck;
+	// }
+
+	// // seems OK, start receiving the message
+	// return super.receiveMessage(m, from);
+	// }
+
+	@Override
+	public int receiveMessage(Message m, DTNHost from) {
+
+		if (m.getProperty("Type") != null && m.getProperty("Type").equals("VACCINE")) {
+			String targetId = (String) m.getProperty("IdToDelete");
+			if (this.hasMessage(targetId)) {
+				this.deleteMessage(targetId, false);
+				System.out.println(
+						"Message " + targetId + " deleted from host " + this.getHost() + " due to VACCINE received");
+			}
+		}
+		int recvCheck = checkReceiving(m);
+		if (recvCheck != RCV_OK) {
+			// Jika pesan ditolak (karena buffer penuh atau sudah pernah diterima), batalkan
+			// eksekusi
+			return recvCheck;
+		}
+
+		if (m.getTo() == this.getHost()) {
+			if (m.getProperty("Type") == null || !m.getProperty("Type").equals("VACCINE")) {
+				Message ackMessage = new Message(this.getHost(), m.getFrom(), "ACK_" + m.getId(), 1);
+				ackMessage.addProperty("Type", "VACCINE");
+				ackMessage.addProperty("IdToDelete", m.getId());
+				ackMessage.setTtl(m.getTtl());
+				this.createNewMessage(ackMessage);
+				System.out.println("ACK sent for message " + m.getId() + " from " + this.getHost());
+			}
+		}
+		return super.receiveMessage(m, from);
+
+	}
 }
